cd17132cf45b233afbaddecdd54d7da4f4bd0e22
Eric <ericinpcola@gmail.com>
2021-05-18 15:59:09 -0500

    Init Commit

---
+++ kaibabot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = '!';
+
+bot.on('message', async message => {
+    
+    console.log(message.content);
+
+    if (message.content == `${prefix}Seto`) {
+        await message.channel.send('Kaiba.');
+    }
+});
+
+bot.login('client key');
